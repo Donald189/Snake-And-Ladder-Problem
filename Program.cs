56836a2bc62ebd75118b900b0c925420409903eb
Donald189 <donaldkarey@gmail.com>
2022-02-22 18:27:46 +0530

    Snake And Ladder

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SnakeAndLadderProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake And Ladder\n");
+
+            //StartGame.StartTheGame();
+
+            //RollingDice.DiceRoll();
+
+            //Options.ChooseOptions();
+
+            ////forGamePlay
+            //Console.WriteLine("Board size?");
+            //int boardsize = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Number of players?");
+            //int players = Convert.ToInt32(Console.ReadLine());
+            //GamePlay g = new GamePlay(boardsize, players);
+            //g.Play();
+
+
+        }
+    }
+}
